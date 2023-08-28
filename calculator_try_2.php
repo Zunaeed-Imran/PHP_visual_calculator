f59f801aa76844cadf6f170a5500fb5817efdda1
imran <zunaeede@gmail.com>
2023-08-28 12:41:01 +0600

    main -button place

--- calculator_try_2.php
+++ calculator_try_2.php
@@ -1,35 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
+
 <body class="p-3 mb-2 bg-secondary text-white">
     <form action="calculator_try_2.php" method="get">
         <div class="row">
-            <div class="col">
-                <button>0</button>
-            </div>
-            <div class="col">
-                <button>1</button>
-            </div>
-            <div class="col">
-                <button>2</button>
-            </div>
-            <div class="col">
-                <button>3</button>
-            </div>
-            <div class="col">
-                <button>4</button>
-            </div>
-            <div class="col">
-                <button>5</button>
-            </div>
-            <div class="col">
-                <button>6</button>
-            </div>
             <div class="col">
                 <button>7</button>
             </div>
@@ -42,18 +23,51 @@
             <div class="col">
                 <button>+</button>
             </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button>4</button>
+            </div>
+            <div class="col">
+                <button>5</button>
+            </div>
+            <div class="col">
+                <button>6</button>
+            </div>
             <div class="col">
                 <button>-</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button>1</button>
+            </div>
+            <div class="col">
+                <button>2</button>
+            </div>
+            <div class="col">
+                <button>3</button>
             </div>
             <div class="col">
                 <button>*</button>
             </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button>0</button>
+            </div>
             <div class="col">
                 <button>/</button>
             </div>
-            
-
+            <div class="col">
+                <button>=</button>
+            </div>
         </div>
+
+
     </form>
 </body>
+
 </html>
