8dd4fd2fb037f9bb246f44d4b1b3cb37cf48163a
imran <zunaeede@gmail.com>
2023-08-28 15:07:43 +0600

    main -calculator -styling

--- calculator_try_2.php
+++ calculator_try_2.php
@@ -12,57 +12,57 @@
     <form action="calculator_try_2.php" method="get">
         <div class="row">
             <div class="col">
-                <button>7</button>
+                <button type="button" class="btn btn-primary btn-block">7</button>
             </div>
             <div class="col">
-                <button>8</button>
+                <button type="button" class="btn btn-primary btn-block">8</button>
             </div>
             <div class="col">
-                <button>9</button>
+                <button type="button" class="btn btn-primary btn-block">9</button>
             </div>
             <div class="col">
-                <button>+</button>
+                <button type="button" class="btn btn-primary btn-block">+</button>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <button>4</button>
+                <button type="button" class="btn btn-primary btn-block">4</button>
             </div>
             <div class="col">
-                <button>5</button>
+                <button type="button" class="btn btn-primary btn-block">5</button>
             </div>
             <div class="col">
-                <button>6</button>
+                <button type="button" class="btn btn-primary btn-block">6</button>
             </div>
             <div class="col">
-                <button>-</button>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col">
-                <button>1</button>
-            </div>
-            <div class="col">
-                <button>2</button>
-            </div>
-            <div class="col">
-                <button>3</button>
-            </div>
-            <div class="col">
-                <button>*</button>
+                <button type="button" class="btn btn-primary btn-block">-</button>
             </div>
         </div>
 
         <div class="row">
             <div class="col">
-                <button>0</button>
+                <button type="button" class="btn btn-primary btn-block">1</button>
             </div>
             <div class="col">
-                <button>/</button>
+                <button type="button" class="btn btn-primary btn-block">2</button>
             </div>
             <div class="col">
-                <button>=</button>
+                <button type="button" class="btn btn-primary btn-block">3</button>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-block">*</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-block">0</button>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-block">/</button>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-block">=</button>
             </div>
         </div>
 
