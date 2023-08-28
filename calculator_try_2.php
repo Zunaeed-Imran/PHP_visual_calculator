9b2381adc557b4c2c5677b29922a57b7f7935fdd
imran <zunaeede@gmail.com>
2023-08-28 12:25:39 +0600

    main -button add

--- calculator_try_2.php
+++ calculator_try_2.php
@@ -6,6 +6,53 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="calculator_try_2.php" method="get">
+        <div class="row">
+            <div class="col">
+                <button>0</button>
+            </div>
+            <div class="col">
+                <button>1</button>
+            </div>
+            <div class="col">
+                <button>2</button>
+            </div>
+            <div class="col">
+                <button>3</button>
+            </div>
+            <div class="col">
+                <button>4</button>
+            </div>
+            <div class="col">
+                <button>5</button>
+            </div>
+            <div class="col">
+                <button>6</button>
+            </div>
+            <div class="col">
+                <button>7</button>
+            </div>
+            <div class="col">
+                <button>8</button>
+            </div>
+            <div class="col">
+                <button>9</button>
+            </div>
+            <div class="col">
+                <button>+</button>
+            </div>
+            <div class="col">
+                <button>-</button>
+            </div>
+            <div class="col">
+                <button>*</button>
+            </div>
+            <div class="col">
+                <button>/</button>
+            </div>
+            
+
+        </div>
+    </form>
 </body>
 </html>
