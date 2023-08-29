1e0144337e6effe37e1beb4134ead58274a8ab47
imran <zunaeede@gmail.com>
2023-08-29 11:46:22 +0600

    main -input -name value

--- try4.php
+++ try4.php
@@ -12,21 +12,21 @@
             <div class="col-md-12">
                 <form action="try4.php" method="$_GET">
                     <input type="text" class="display form-control" name="display">
-                    <input type="button" class="btn btn-primary" name="c" value="C">
-                    <input type="button" class="btn btn-primary" name="c" value="X">
-                    <input type="button" class="btn btn-primary" name="c" value="/"><br>
-                    <input type="button" class="btn btn-primary" name="c" value="7">
-                    <input type="button" class="btn btn-primary" name="c" value="8">
-                    <input type="button" class="btn btn-primary" name="c" value="9"><br>
-                    <input type="button" class="btn btn-primary" name="c" value="4">
-                    <input type="button" class="btn btn-primary" name="c" value="5">
-                    <input type="button" class="btn btn-primary" name="c" value="6"><br>
-                    <input type="button" class="btn btn-primary" name="c" value="1">
-                    <input type="button" class="btn btn-primary" name="c" value="2">
-                    <input type="button" class="btn btn-primary" name="c" value="3"><br>
-                    <input type="button" class="btn btn-primary" name="c" value="+">
-                    <input type="button" class="btn btn-primary" name="c" value="-">
-                    <input type="button" class="btn btn-primary" name="c" value="*">
+                    <input type="button" class="btn btn-danger" name="c" value="C">
+                    <input type="button" class="btn btn-primary" name="equal" value="=">
+                    <input type="button" class="btn btn-primary" name="devide" value="/"><br>
+                    <input type="button" class="btn btn-primary" name="num" value="7">
+                    <input type="button" class="btn btn-primary" name="num" value="8">
+                    <input type="button" class="btn btn-primary" name="num" value="9"><br>
+                    <input type="button" class="btn btn-primary" name="num" value="4">
+                    <input type="button" class="btn btn-primary" name="num" value="5">
+                    <input type="button" class="btn btn-primary" name="num" value="6"><br>
+                    <input type="button" class="btn btn-primary" name="num" value="1">
+                    <input type="button" class="btn btn-primary" name="num" value="2">
+                    <input type="button" class="btn btn-primary" name="num" value="3"><br>
+                    <input type="button" class="btn btn-primary" name="plus" value="+">
+                    <input type="button" class="btn btn-primary" name="subtrac" value="-">
+                    <input type="button" class="btn btn-primary" name="multi" value="*">
                 </form>
 
             </div>
