5201423b3b88d34170b03bd8c70a71cf4bbeec36
imran <zunaeede@gmail.com>
2023-08-29 14:32:39 +0600

    main -boostrap margin -boostrap class -margin class

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 <body class="p-3 mb-2 bg-secondary text-white" style="--bs-bg-opacity: .8;">
 
 ``` 
+2. magrin in boostrap class
+```
+<div class="m-3">Margin on all sides</div>
+<div class="mt-4">Top margin</div>
+<div class="py-2">Padding on top and bottom</div>
+<div class="ms-auto">Automatic left margin</div>
+
+```
--- try4.php
+++ try4.php
@@ -12,21 +12,21 @@
         <div class="col-md-12">
             <form action="try4.php" method="$_GET">
                 <input type="text" class="display form-control" name="display">
-                <input type="button" class="btn btn-danger gap-3" name="c" value="C">
-                <input type="button" class="btn btn-warning gap-3" name="equal" value="=">
-                <input type="button" class="btn btn-success ml-3" name="devide" value="/"><br>
-                <input type="button" class="btn btn-primary ml-3" name="num" value="7">
-                <input type="button" class="btn btn-primary ml-2" name="num" value="8">
-                <input type="button" class="btn btn-primary ml-2" name="num" value="9"><br>
-                <input type="button" class="btn btn-primary ml-2" name="num" value="4">
-                <input type="button" class="btn btn-primary ml-2" name="num" value="5">
-                <input type="button" class="btn btn-primary ml-2" name="num" value="6"><br>
-                <input type="button" class="btn btn-primary ml-2" name="num" value="1">
-                <input type="button" class="btn btn-primary ml-2" name="num" value="2">
-                <input type="button" class="btn btn-primary ml-4" name="num" value="3"><br>
-                <input type="button" class="btn btn-success ml-4" name="plus" value="+">
-                <input type="button" class="btn btn-success ml-4" name="subtrac" value="-">
-                <input type="button" class="btn btn-success ml-4" name="multi" value="*">
+                <input type="button" class="btn btn-danger me-2" name="c" value="C">
+                <input type="button" class="btn btn-warning mr-2" name="equal" value="=">
+                <input type="button" class="btn btn-success mr-2" name="devide" value="/"><br>
+                <input type="button" class="btn btn-primary mr-2" name="num" value="7">
+                <input type="button" class="btn btn-primary mr-2" name="num" value="8">
+                <input type="button" class="btn btn-primary mr-2" name="num" value="9"><br>
+                <input type="button" class="btn btn-primary mr-2" name="num" value="4">
+                <input type="button" class="btn btn-primary mr-2" name="num" value="5">
+                <input type="button" class="btn btn-primary mr-2" name="num" value="6"><br>
+                <input type="button" class="btn btn-primary mr-2" name="num" value="1">
+                <input type="button" class="btn btn-primary mr-2" name="num" value="2">
+                <input type="button" class="btn btn-primary mr-4" name="num" value="3"><br>
+                <input type="button" class="btn btn-success mr-4" name="plus" value="+">
+                <input type="button" class="btn btn-success mr-4" name="subtrac" value="-">
+                <input type="button" class="btn btn-success mr-4" name="multi" value="*">
             </form>
 
         </div>
