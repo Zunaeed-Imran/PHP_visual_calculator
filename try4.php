cacc8c9bcb0e5843e8dedcf55d4905ec0f90e3ba
imran <zunaeede@gmail.com>
2023-08-29 11:41:22 +0600

    main -button style -background color -opacity background

--- try4.php
+++ try4.php
@@ -6,8 +6,34 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="p-3 mb-2 bg-success text-white" style="--bs-bg-opacity: .8;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="try4.php" method="$_GET">
+                    <input type="text" class="display form-control" name="display">
+                    <input type="button" class="btn btn-primary" name="c" value="C">
+                    <input type="button" class="btn btn-primary" name="c" value="X">
+                    <input type="button" class="btn btn-primary" name="c" value="/"><br>
+                    <input type="button" class="btn btn-primary" name="c" value="7">
+                    <input type="button" class="btn btn-primary" name="c" value="8">
+                    <input type="button" class="btn btn-primary" name="c" value="9"><br>
+                    <input type="button" class="btn btn-primary" name="c" value="4">
+                    <input type="button" class="btn btn-primary" name="c" value="5">
+                    <input type="button" class="btn btn-primary" name="c" value="6"><br>
+                    <input type="button" class="btn btn-primary" name="c" value="1">
+                    <input type="button" class="btn btn-primary" name="c" value="2">
+                    <input type="button" class="btn btn-primary" name="c" value="3"><br>
+                    <input type="button" class="btn btn-primary" name="c" value="+">
+                    <input type="button" class="btn btn-primary" name="c" value="-">
+                    <input type="button" class="btn btn-primary" name="c" value="*">
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
 </body>
 </html>
 
