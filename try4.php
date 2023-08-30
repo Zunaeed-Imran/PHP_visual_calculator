edf4906f84cd09f761f55094ef618f0ec58ebaa8
imran <zunaeede@gmail.com>
2023-08-30 11:13:53 +0600

    main -give condation -calculator condation

--- try4.php
+++ try4.php
@@ -14,9 +14,10 @@
         <div class="col-md-12">
             <form action="try4.php" method="$_GET">
                 <input type="text" class="display form-control" name="display">
-                <input type="button" class="btn btn-danger me-2" name="c" value="C">
-                <input type="button" class="btn btn-warning me-2" name="equal" value="=">
-                <input type="button" class="btn btn-success me-2" name="devide" value="/"><br>
+                <input type="button" class="btn btn-success me-2" name="symbol" value="+">
+                <input type="button" class="btn btn-success me-2" name="symbol" value="-">
+                <input type="button" class="btn btn-success me-2" name="symbol" value="*">
+                <input type="button" class="btn btn-success me-2" name="symbol" value="/"><br>
                 <input type="button" class="btn btn-primary me-2" name="num" value="7">
                 <input type="button" class="btn btn-primary me-2" name="num" value="8">
                 <input type="button" class="btn btn-primary me-2" name="num" value="9"><br>
@@ -26,9 +27,11 @@
                 <input type="button" class="btn btn-primary me-2" name="num" value="1">
                 <input type="button" class="btn btn-primary me-2" name="num" value="2">
                 <input type="button" class="btn btn-primary me-2" name="num" value="3"><br>
-                <input type="button" class="btn btn-success me-2" name="plus" value="+">
-                <input type="button" class="btn btn-success me-2" name="subtrac" value="-">
-                <input type="button" class="btn btn-success me-2" name="multi" value="*">
+                <input type="button" class="btn btn-danger me-2" name="c" value="C">           
+                <input type="button" class="btn btn-primary me-2" name="num" value="0">
+                <input type="button" class="btn btn-warning me-2" name="equal" value="=">               
+                
+                
             </form>
 
         </div>
@@ -42,7 +45,7 @@
         document.addEventListener("DOMContentLoaded", function() {
             const display = document.querySelector(".display");
 
-            document.querySelectorAll("[name='num'],[name='plus'],[name='subtrac'],[name='multi'],[name='devide'],[name='equal'],[name='c']").forEach(button => {
+            document.querySelectorAll("[name='num'],[name='symbol']").forEach(button => {
                 button.addEventListener("click", function() {
                     display.value += button.value;
                 });
@@ -50,15 +53,33 @@
 
             // Add similar event listeners for other buttons (operators, clear, etc.).
         });
-
-    </script>
+ </script>
 </body>
 </html>
 
 
 <?php
 
+function calculate($num, $symbol)
+{
+    if ($symbol === '+') {
+        echo $num + $num;
+    } elseif ($symbol === '-') {
+        echo $num - $num;
+    } elseif ($symbol === '*') {
+        echo $num * $num;
+    } elseif ($symbol === '/') {
+        echo $num / $num;
+    } 
+}
 
+if (isset($_REQUEST['num']) && isset($_REQUEST['c']) && isset($_REQUEST['equal'])  && isset($_REQUEST['symbol'])) {
+    $num = $_REQUEST['num'];
+    $symbol = $_REQUEST['symbol'];
+    $c = $_REQUEST['c'];
+    $equal = $_REQUEST['equal'];
+    calculate($num, $symbol); 
+}
 
 
 ?>
