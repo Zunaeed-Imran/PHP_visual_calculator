25f96690723d05a00301b1d8a50a2eb0d7c0ec87
imran <zunaeede@gmail.com>
2023-09-07 09:35:54 +0600

    pracb -practice

--- draft2.php
+++ draft2.php
@@ -6,8 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <button></button>
+    <div class="calculator">
+        <input type="text" id="display" class="display"><br>
+        <button class="button" onclick="display('7')">7</button>
+        <button class="button" onclick="display('8')">8</button>
+        <button class="button" onclick="display('9')">9</button><br>
+        <button class="button" onclick="display('4')">4</button>
+        <button class="button" onclick="display('5')">5</button>
+        <button class="button" onclick="display('6')">6</button><br>
+        <button class="button" onclick="display('1')">1</button>
+        <button class="button" onclick="display('2')">2</button>
+        <button class="button" onclick="display('3')">3</button><br>
+        <button class="button" onclick="clear('c')">C</button>
+        <button class="button" onclick="display('0')">0</button>
+        <button class="button" onclick="calculate('=')">=</button><br>
+        <button class="button" onclick="display('+')">+</button>
+        <button class="button" onclick="display('-')">-</button>
+        <button class="button" onclick="display('*')">*</button>
+        <button class="button" onclick="display('/')">/</button>
     </div>
 
 
@@ -15,7 +31,24 @@
 
     <script>
 
+        function Display(value) {
+            document.querySelector('display').value += value;
+        }
 
+        function clearDisplay() {
+            document.querySelector('display').value = '';
+        }
+
+        function calculate() {
+            try {
+                const result = eval(document.querySelector('display').value);
+                document.querySelector('display').value = result;
+            } catch (error) {
+                document.querySelector('display').value = 'Error';
+            }
+        }
+        
     </script>
+
 </body>
 </html>
--- try5.php
+++ try5.php
@@ -31,19 +31,19 @@
     <script>
 
         function Display(value) {
-            document.getElementById('display').value += value;
+            document.querySelector('display').value += value;
         }
 
         function clearDisplay() {
-            document.getElementById('display').value = '';
+            document.querySelector('display').value = '';
         }
 
         function calculate() {
             try {
-                const result = eval(document.getElementById('display').value);
-                document.getElementById('display').value = result;
+                const result = eval(document.querySelector('display').value);
+                document.querySelector('display').value = result;
             } catch (error) {
-                document.getElementById('display').value = 'Error';
+                document.querySelector('display').value = 'Error';
             }
         }
         
