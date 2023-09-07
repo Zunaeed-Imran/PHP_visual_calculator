c4195d6764612bd39d11b04f1518d35f8af073ec
imran <zunaeede@gmail.com>
2023-09-07 14:15:49 +0600

    main - calculator -eval without

---
+++ 6_calculator.php
@@ -0,0 +1,37 @@
+<!-- // CALCULATOR WITHOUT eval function -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <input type="text" class="display" id="display"><br>
+        <button class="button" onclick="display('9')">9</button>
+        <button class="button" onclick="display('8')">8</button>
+        <button class="button" onclick="display('7')">7</button><br>
+        <button class="button" onclick="display('6')">6</button>
+        <button class="button" onclick="display('5')">5</button>
+        <button class="button" onclick="display('4')">4</button><br>
+        <button class="button" onclick="display('3')">3</button>
+        <button class="button" onclick="display('2')">2</button>
+        <button class="button" onclick="display('1')">1</button><br>
+        <button class="button" onclick="cle('c')">C</button>
+        <button class="button" onclick="display('0')">0</button>
+        <button class="button" onclick="calculate()">=</button><br>
+        <button class="button" onclick="display('+')">+</button>
+        <button class="button" onclick="display('-')">-</button>
+        <button class="button" onclick="display('*')">*</button><br>
+        <button class="button" onclick="display('/')">/</button>
+    </div>
+
+
+
+    <script>
+
+    </script>
+</body>
+</html>
