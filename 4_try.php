a3f8c193fdeeec226134accc80f0ea5c10ea8333
imran <zunaeede@gmail.com>
2023-09-07 18:09:42 +0600

    main - test - text remove.

--- 4_try.php
+++ 4_try.php
@@ -7,9 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
-
-// this will command test
-
 <body class="p-3 mb-2 bg-secondary text-white" style="--bs-bg-opacity: .8;">
 <div class="container">
     <div class="row">
