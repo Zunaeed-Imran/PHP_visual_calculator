77bd5ab93d4cbd5029e4d6ef09a1f27b4ffbf4c8
imran <zunaeede@gmail.com>
2024-10-30 21:50:27 +0600

    main - command what i don't need.

--- note_javascript.php
+++ note_javascript.php
@@ -3,33 +3,33 @@
 {/* <input name="other" /> */}
 {/* <input name="num" /> */} // selected
 {/* <input name="symbol" /> */} // selected
-document.querySelectorAll("[name='num'],[name='symbol']")
-    .forEach(button => {
-        button.addEventListener("click", function() {
-            display.value += this.value;
-        });
-    });
-{/* <input name="other" /> */}
-{/* <input name="num" /> */} // selected
-{/* <input name="symbol" /> */}
-document.querySelector("[name='num'],[name='symbol']")
-    .addEventListener("click", function() {
-        display.value += this.value;
-    });
-{/* <button /> */}
-{/* <input name="other" /> */} // selected
-{/* <input class="btn" /> */} // selected
-document.querySelectorAll("input");
-{/* <input id="btn" /> */}
-{/* <input class="btn btn-success me-2" /> */} // selected
-{/* <input class="btn" /> */} // selected
-document.querySelectorAll(".btn");
-{/* <input class="btn" /> */}
-{/* <input id="btn" /> */} // selected
-document.querySelectorAll("#btn");
-{/* <input class="btn btn-success me-2" /> */}
-{/* <input class="btn" /> */} // selected
-document.querySelector("[class=btn]");
+// document.querySelectorAll("[name='num'],[name='symbol']")
+    // .forEach(button => {
+    //     button.addEventListener("click", function() {
+    //         display.value += this.value;
+    //     });
+    // });
+// {/* <input name="other" /> */}
+// {/* <input name="num" /> */} // selected
+// {/* <input name="symbol" /> */}
+// document.querySelector("[name='num'],[name='symbol']")
+//     .addEventListener("click", function() {
+//         display.value += this.value;
+//     });
+// {/* <button /> */}
+// {/* <input name="other" /> */} // selected
+// {/* <input class="btn" /> */} // selected
+// document.querySelectorAll("input");
+// {/* <input id="btn" /> */}
+// {/* <input class="btn btn-success me-2" /> */} // selected
+// {/* <input class="btn" /> */} // selected
+// document.querySelectorAll(".btn");
+// {/* <input class="btn" /> */}
+// {/* <input id="btn" /> */} // selected
+// document.querySelectorAll("#btn");
+// {/* <input class="btn btn-success me-2" /> */}
+// {/* <input class="btn" /> */} // selected
+// document.querySelector("[class=btn]");
 
 
 ?>
